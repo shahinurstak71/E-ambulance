9aad1c20f9394ac8c65a0275d110b75bd1e0e5a3
abircse45 <abiruodacse@gmail.com>
2023-01-16 20:46:00 +0600

    updated

--- lib/screens/mappage.dart
+++ lib/screens/mappage.dart
@@ -16,6 +16,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 //
@@ -108,12 +109,26 @@ class MapSampleState extends State<MapSample> {
     'images/marker3.png', 'images/marker.png' , 'images/motorcycle.png' ,];
   dynamic  ? latww;
   dynamic ? longwww;
+  dynamic ? name;
+  dynamic ? email;
+  dynamic ? no;
+  dynamic ? pf;
+  dynamic ? address;
+  dynamic ? mobile;
+  bool showDetail=false;
+
   Future<void> _goToTheLake() async {
 
    await FirebaseFirestore.instance.collection("driver").get().then((value) {
       value.docs.forEach((element) {
         latww = double.parse(element.data()['lon']);
         longwww = double.parse(element.data()['lat']);
+        name = element.data()['name'];
+        email = element.data()['email'];
+        no = element.data()['ambulance_no'];
+        pf = element.data()['photo'];
+        address = element.data()['set_address'];
+        mobile = element.data()['mobile'];
         print("ELEMENT__${element.data()['lon']}");
       });
     });
@@ -141,7 +156,7 @@ class MapSampleState extends State<MapSample> {
   Future<Uint8List>  getBytesFromAssets(String path , int width) async
   {
     ByteData data = await rootBundle.load(path);
-    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List() , targetHeight:33 );
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List() , targetHeight:45 );
     ui.FrameInfo fi = await codec.getNextFrame() ;
     return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!.buffer.asUint8List();
   }
@@ -155,8 +170,8 @@ class MapSampleState extends State<MapSample> {
 
       final ui.Codec markerImageCodec = await instantiateImageCodec(
         image!.buffer.asUint8List(),
-        targetHeight: 80,
-        targetWidth: 80,
+        targetHeight: 100,
+        targetWidth: 100,
       );
       final FrameInfo frameInfo = await markerImageCodec.getNextFrame();
       final ByteData? byteData = await frameInfo.image.toByteData(
@@ -175,10 +190,21 @@ class MapSampleState extends State<MapSample> {
       //       ),
       //     ));
       marker.add(Marker(
+        onTap: (){
+          setState(() {
+            showDetail=true;
+          });
+        },
         icon: BitmapDescriptor.fromBytes(resizedMarkerImageBytes),
 
         markerId:  MarkerId(longwww.toString()),
         position: LatLng(longwww, latww!),
+        infoWindow: InfoWindow(
+
+          title: "${name}"
+
+
+        )
       ));
       setState(() {
 
@@ -198,22 +224,241 @@ class MapSampleState extends State<MapSample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: GoogleMap(
-          mapType: MapType.normal,
-          markers: marker,
-          initialCameraPosition: _kGooglePlex,
-          myLocationEnabled: true,
-          myLocationButtonEnabled: true,
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-            setState(() {});
-            _goToTheLake();
-          },
-        ),
+      body: Stack(
+        children: [
+          Expanded(
+            child: GoogleMap(
+              mapType: MapType.normal,
+              markers: marker,
+              initialCameraPosition: _kGooglePlex,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+                setState(() {});
+                _goToTheLake();
+              },
+            ),
+          ),
+
+          Visibility(
+            visible: showDetail,
+
+            child: Positioned(
+                bottom: 10,
+                left: 10,
+                right: 10,
+                child: Card(
+                  child: Stack(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(5.0),
+                          child: Column(
+                            crossAxisAlignment:
+                            CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+
+                              SizedBox(height: 5,),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  CircleAvatar(
+                                    radius: 23,
+                                    backgroundImage: NetworkImage(
+                                      "$pf"
+                                    ),
+                                  ),
+                                  SizedBox(width: 12,),
+                                  Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: Text("${name}",style: TextStyle(
+                                      fontSize: 23,color: Colors.black
+                                    ),),
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Text("${email}",style: TextStyle(
+                                    fontSize: 16,color: Colors.black
+                                ),),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Text("Ambulance No.${no}",style: TextStyle(
+                                    fontSize: 17,color: Colors.black
+                                ),),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Text("Address :  ${address}",style: TextStyle(
+                                    fontSize: 17,color: Colors.black
+                                ),),
+                              ),
+                              SizedBox(height: 7,),
+
+                              Container(
+                                margin: EdgeInsets.zero,
+                                padding: EdgeInsets.zero,
+                                width: double.infinity,
+                                child: Card(
+                                  elevation: 5,
+                                  //color: kPrimaryDarkenColor,
+                                  margin: EdgeInsets.zero,
+                                  child: InkWell(
+                                    onTap: ()  {
+                                      _makePhoneCall("+88${mobile}");
+
+
+                                    },
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          Text("Call",style: TextStyle(color: Colors.black,fontWeight: FontWeight.w600),),
+                                          SizedBox(
+                                            width:10,
+                                          ),
+                                          // Icon(
+                                          //   Icons.send_outlined,
+                                          //   color: kPrimaryDarkenColor,
+                                          // ),
+
+
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+
+                              SizedBox(height: 10,),
+
+                            ],
+                          ),
+                        ),
+                        // Container(
+                        //   padding: EdgeInsets.all(5.0),
+                        //   child: Column(
+                        //     crossAxisAlignment:
+                        //     CrossAxisAlignment.start,
+                        //     children: [
+                        //       Row(
+                        //         children: [
+                        //           Container(
+                        //             decoration: BoxDecoration(
+                        //               shape: BoxShape.circle,
+                        //               border:  Border.all(
+                        //                 color: Colors.green,
+                        //                 width: 2.0,
+                        //               ),
+                        //             ),
+                        //             child: ClipOval(
+                        //               child: FadeInImage.assetNetwork(
+                        //                 placeholder:
+                        //                 Images.placeholder,
+                        //                 image:
+                        //                 "${Constants.IMG_URL}${singleProfile!.photo!}",
+                        //                 width: 60,
+                        //                 height: 60,
+                        //                 fit: BoxFit.cover,
+                        //                 imageErrorBuilder: (context,
+                        //                     error, stackTrace) {
+                        //                   return Image.asset(
+                        //                       Images.placeholder,
+                        //                       width: 60,
+                        //                       height: 60,
+                        //                       fit: BoxFit.cover);
+                        //                 },
+                        //               ),
+                        //             ),
+                        //           ),
+                        //           SizedBox(width: 5,),
+                        //           Column(
+                        //             crossAxisAlignment: CrossAxisAlignment.start,
+                        //             children: [
+                        //               Text(
+                        //                   "${singleProfile!.fullName!}"),
+                        //               Text(
+                        //                   "${singleProfile!.ocacupation!}"),
+                        //             ],
+                        //           ),
+                        //         ],
+                        //       ),
+                        //       SizedBox(height: 5,),
+                        //       Text("${singleProfile!.email!}"),
+                        //       SizedBox(height: 5,),
+                        //       Text("${singleProfile!.mobile!}"),
+                        //       SizedBox(height: 5,),
+                        //       Text("${singleProfile!.address!}"),
+                        //
+                        //       Container(
+                        //         margin: EdgeInsets.zero,
+                        //         padding: EdgeInsets.zero,
+                        //         width: double.infinity,
+                        //         child: Card(
+                        //           elevation: 5,
+                        //           //color: kPrimaryDarkenColor,
+                        //           margin: EdgeInsets.zero,
+                        //           child: InkWell(
+                        //             onTap: () async {
+                        //               var email=await MyPrefs.getEmail();
+                        //               Navigator.push(context, MaterialPageRoute(builder: (context)=>SingleChatScreen(email: singleProfile!.email!, name: singleProfile!.fullName!,myEmail: email,)));
+                        //             },
+                        //             child: Padding(
+                        //               padding: const EdgeInsets.all(8.0),
+                        //               child: Row(
+                        //                 mainAxisAlignment: MainAxisAlignment.center,
+                        //                 children: <Widget>[
+                        //                   Text("Send Message",style: TextStyle(color: kPrimaryDarkenColor),),
+                        //                   SizedBox(
+                        //                     width:10,
+                        //                   ),
+                        //                   Icon(
+                        //                     Icons.send_outlined,
+                        //                     color: kPrimaryDarkenColor,
+                        //                   ),
+                        //
+                        //
+                        //                 ],
+                        //               ),
+                        //             ),
+                        //           ),
+                        //         ),
+                        //       )
+                        //
+                        //     ],
+                        //   ),
+                        // ),
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: InkWell(
+                              onTap: (){
+                                setState(() {
+                                  showDetail=false;
+                                });
+                              },
+                              child: Icon(Icons.clear,color: Colors.grey,)),
+                        ),
+                      ]
+                  ),
+                )),
+          )
+        ],
       ),
     );
   }
+  Future<void> _makePhoneCall(String phoneNumber) async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    await launchUrl(launchUri);
+  }
+
 }
 
 
